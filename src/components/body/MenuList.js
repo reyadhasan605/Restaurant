f407fc5151ea5f72490a5c01dad9220edbed9dc7
Reyad <reyadhasan605@gmail.com>
2021-08-12 12:44:40 +0600

    Designing menu item

--- src/components/body/MenuList.js
+++ src/components/body/MenuList.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Card, CardTitle, CardText, CardImg, CardImgOverlay } from 'reactstrap';
 
 let MenuList = (props) => {
-    console.log(props);
     return (
         <div>
+
+            <Card inverse style={{ margin: '10px' }}>
+                <CardImg width="100%" src={props.dish.image} alt={props.dish.name} />
+                <CardImgOverlay>
+                    <CardTitle tag="h5">{props.dish.name}</CardTitle>
+                    <CardText>
+                        <small className="text-muted" style={{ color: 'blue' }}>Last updated 3 mins ago</small>
+                    </CardText>
+                </CardImgOverlay>
+            </Card>
 
         </div>
     );
